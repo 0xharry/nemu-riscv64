8557f071a647cec3238f0e1929d9d3bfdc630bd9
tracer-ics2020 <tracer@njuics.org>
2021-05-17 14:58:43 +0800

    >  compile ustc    Static hostname: HASEE          Icon name: computer-laptop            Chassis: laptop         Machine ID: 028aba91a4974ce08e9cf5508e313209            Boot ID: 01089083a81642fd87dfd89cc487b839   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  14:58:43 up 3 days,  2:11,  5 users,  load average: 0.06, 0.19, 0.26

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -89,7 +89,7 @@ static int cmd_help(char *args) {
   if (arg == NULL) {
     /* no argument given */
     for (i = 0; i < NR_CMD; i ++) {
-      printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
+      printf("%s\t- %s\n", cmd_table[i].name, cmd_table[i].description);
     }
   }
   else {
