a195c75eb53c7896749d79a24703f7c73fc9ae44
tracer-ics2020 <tracer@njuics.org>
2021-05-17 14:59:02 +0800

    >  compile ustc    Static hostname: HASEE          Icon name: computer-laptop            Chassis: laptop         Machine ID: 028aba91a4974ce08e9cf5508e313209            Boot ID: 01089083a81642fd87dfd89cc487b839   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  14:59:02 up 3 days,  2:12,  5 users,  load average: 0.12, 0.20, 0.26

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -85,7 +85,7 @@ void cpu_exec(uint64_t n) {
 
     g_nr_guest_instr ++;
     // 测试si好不好使
-    printf("cpu_exec n=%lu\npc=%lux\n",n,cpu.pc);
+    printf("cpu_exec n=%lu\npc=%lx\n",n,cpu.pc);
 
 #ifdef DEBUG
     asm_print(this_pc, seq_pc - this_pc, n < MAX_INSTR_TO_PRINT);
