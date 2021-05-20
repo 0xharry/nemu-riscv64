180def732e0c6ea7646f8e70fc4fede3637261df
tracer-ics2020 <tracer@njuics.org>
2021-05-20 23:05:29 +0800

    >  compile ustc    Static hostname: HASEE          Icon name: computer-laptop            Chassis: laptop         Machine ID: 028aba91a4974ce08e9cf5508e313209            Boot ID: 01089083a81642fd87dfd89cc487b839   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  23:05:29 up 6 days, 10:18,  4 users,  load average: 0.59, 0.66, 0.60

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -19,7 +19,7 @@ void isa_reg_display() {
 word_t isa_reg_str2val(const char *s, bool *success) {
   for(int i=0; i<32; ++i)
   {
-    if(strncmp("pc", s, 2)==1)
+    if(strncmp("pc", s, 3)==1)
     {
       *success = true;
       return cpu.pc;
