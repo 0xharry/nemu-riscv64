d3fff663322dc0a6984d4b15793776d4dc9dea28
tracer-ics2020 <tracer@njuics.org>
2021-06-01 17:56:42 +0800

    >  compile ustc    Static hostname: HASEE          Icon name: computer-laptop            Chassis: laptop         Machine ID: 028aba91a4974ce08e9cf5508e313209            Boot ID: 5b2e07037dc94856a120e19a72818f05   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  17:56:42 up 1 day,  8:01,  5 users,  load average: 1.24, 1.06, 1.22

--- nemu/src/isa/riscv64/exec/control.h
+++ nemu/src/isa/riscv64/exec/control.h
@@ -2,11 +2,12 @@
 // x[rd] = pc+4; pc += sext(offset)
 static inline sword_t jal_offset_decode(word_t imm)
 {
-  sword_t simm = ((sword_t)(
-                ((imm & (word_t)0x00080000)<<1 ) |\
-                ((imm & (word_t)0x000000FF)<<12) |\
-                ((imm & (word_t)0x00000100)<<3 ) |\
-                ((imm & (word_t)0x0007FE00)>>8 )))<<43>>43;
+  sword_t simm = ((sword_t)(((imm & (word_t)0x00080000) << 1) |
+                            ((imm & (word_t)0x000000FF) << 12) |
+                            ((imm & (word_t)0x00000100) << 3) |
+                            ((imm & (word_t)0x0007FE00) >> 8)))
+                     << 43 >>
+                 43; //sizeof(word_t)-21 = 64-21 = 43
   return simm;
 }
 /* offset[20|10:1|11|19:12]
@@ -20,9 +21,8 @@ static inline sword_t jal_offset_decode(word_t imm)
 
 static inline def_EHelper(jal)
 {
-  rtl_addi(s, ddest, &s->seq_pc, 0); // x[rd] = pc+4
-  rtl_j(s, cpu.pc + jal_offset_decode(id_src1->imm));   // pc += sext(offset)
-  printf("%lx\t%lx\t%lx\n",jal_offset_decode(id_src1->imm), cpu.pc, s->jmp_pc);
+  rtl_addi(s, ddest, &s->seq_pc, 0);                  // x[rd] = pc+4
+  rtl_j(s, cpu.pc + jal_offset_decode(id_src1->imm)); // pc += sext(offset)
   print_asm_template2(jal);
 }
 
@@ -39,7 +39,7 @@ static inline def_EHelper(jal)
 // x[rd]=t
 static inline def_EHelper(jalr)
 {
-  rtl_j(s, (*dsrc1+id_src2->imm)&~1);
+  rtl_j(s, (*dsrc1 + id_src2->imm) & ~1);
   rtl_addi(s, ddest, rz, s->seq_pc);
   print_asm_template2(jalr);
 }
