5bb86e3e5ffe7d8239c19857212edab2ab450a84
tracer-ics2020 <tracer@njuics.org>
2021-06-06 19:27:43 +0800

    >  compile ustc    Static hostname: HASEE          Icon name: computer-laptop            Chassis: laptop         Machine ID: 028aba91a4974ce08e9cf5508e313209            Boot ID: 5b2e07037dc94856a120e19a72818f05   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  19:27:44 up 6 days,  9:32,  4 users,  load average: 0.83, 0.99, 1.46

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -9,7 +9,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     if(!difftest_check_reg(regs[i], pc, ref_r->gpr[i]._64, cpu.gpr[i]._64))
       return false;
   }
-  return true;
+  return difftest_check_reg("pc", pc, ref_r->pc, cpu.pc);
 }
 
 void isa_difftest_attach() {
