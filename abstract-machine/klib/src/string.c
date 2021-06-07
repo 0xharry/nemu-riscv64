1cb64c95e46c8b3b739fd7450799009e0b1252d2
tracer-ics2020 <tracer@njuics.org>
2021-06-07 18:32:35 +0800

    >  run ustc    Static hostname: HASEE          Icon name: computer-laptop            Chassis: laptop         Machine ID: 028aba91a4974ce08e9cf5508e313209            Boot ID: 5b2e07037dc94856a120e19a72818f05   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-53-generic       Architecture: x86-64  18:32:35 up 7 days,  8:37,  5 users,  load average: 0.54, 0.56, 0.51

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -7,19 +7,55 @@ size_t strlen(const char *s) {
   return 0;
 }
 
+/* strcpy, strncpy - copy a string
+ * The  strcpy() function copies the string pointed to by src, including the termi‐
+ * nating null byte ('\0'), to the buffer pointed to by dest.  The strings may  not
+ * overlap,  and  the  destination  string dest must be large enough to receive the
+ * copy.
+ */
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  if(dst != NULL && src != NULL) do{
+    *dst = *src;
+  } while(src!='\0');
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
   return NULL;
 }
 
+/* strcat - concatenate two strings
+ * the strcat() function appends the src string to the dest string, overwriting the  
+ * terminating null byte ('\\0') at the end of dest, and  then  adds  a  terminating  
+ * null  byte.
+ * The strings may not overlap, and the dest string must have enough space  for  the 
+ * result.  If dest is not large enough, program behavior  is  unpredictable;
+ */
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  char* end_of_dst=dst;
+  char* p_src = src;
+  for(; *end_of_dst != '\0'; ++end_of_dst);
+  for(; *p_src != '\0'; ++end_of_dst, ++p_src) {
+    *end_of_dst = *p_src;
+  }
+  *(end_of_dst+1) = '\0';
+  return dst;
 }
 
+
+/* strcmp - compare two strings
+ * The  strcmp()  function  compares  the two strings s1 and s2.  The locale is not
+ * taken into account (for a locale-aware comparison, see strcoll(3)).  It  returns
+ * an  integer  less  than,  equal to, or greater than zero if s1 is found, respec‐
+ * tively, to be less than, to match, or be greater than s2.
+ */
 int strcmp(const char* s1, const char* s2) {
+  int ret = 0;
+  while(*s1!='\0' || *s2!='\0') {
+    ret = *s1-*s2;
+    if(ret != 0)
+      return ret;
+  }
   return 0;
 }
 
@@ -27,8 +63,16 @@ int strncmp(const char* s1, const char* s2, size_t n) {
   return 0;
 }
 
+/* 
+ * memset - fill memory with a constant byte
+ * The  memset()  function fills the first n bytes of the memory area pointed to
+ * by s with the constant byte c.
+ */
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  for(int i=0; i<n; ++i) {
+    *(char *)v = (char) c;
+  }
+  return v;
 }
 
 void* memmove(void* dst,const void* src,size_t n) {
