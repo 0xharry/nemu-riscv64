0b500a2c28e26615a5cd7fa57a38028747f5adb6
tracer-ics2020 <tracer@njuics.org>
2021-06-09 20:08:54 +0800

    >  compile ustc    Static hostname: HASEE          Icon name: computer-laptop            Chassis: laptop         Machine ID: 028aba91a4974ce08e9cf5508e313209            Boot ID: a6b4bde7f0ad422682b7dba241d96e66   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  20:08:54 up  4:15,  1 user,  load average: 0.77, 0.59, 0.51

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -11,35 +11,24 @@ int printf(const char *fmt, ...) {
   return 0;
 }
 
-/* 
- * The  functions  vprintf(),  vfprintf(),  vdprintf(),  vsprintf(),   vs‐
- * nprintf()   are   equivalent  to  the  functions  printf(),  fprintf(),
- * dprintf(), sprintf(), snprintf(), respectively, except  that  they  are
- * called with a va_list instead of a variable number of arguments.  These
- * functions do not call the va_end macro.  Because they invoke the va_arg
- * macro, the value of ap is undefined after the call.
- */
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
-}
-
 char* itos_dec(int num, char* str) {
   if(str == NULL) {
     // str = (char*) malloc(12); // 最长 int_min 10位十进制整数+负号+终止符号=12位
     // if(str == NULL)
       assert(0);
   }
-  int i=0;
+  int i = 0;
+  int neg = 1;
   if(num<0){
     str[i++] = '-';
-	num = -num;
+    neg = -1;
   }
 
   int r;
   do {
     r = num%10;
     num /= 10;
-    str[i++] = '0'+r;
+    str[i++] = '0'+neg*r;
   } while (num);
   str[i] = '\0';
 
@@ -59,6 +48,60 @@ char* itos_dec(int num, char* str) {
   }
   return str;
 }
+
+/* 
+ * The  functions  vprintf(),  vfprintf(),  vdprintf(),  vsprintf(),   vs‐
+ * nprintf()   are   equivalent  to  the  functions  printf(),  fprintf(),
+ * dprintf(), sprintf(), snprintf(), respectively, except  that  they  are
+ * called with a va_list instead of a variable number of arguments.  These
+ * functions do not call the va_end macro.  Because they invoke the va_arg
+ * macro, the value of ap is undefined after the call.
+ */
+int vsprintf(char *out, const char *fmt, va_list p_fmt) {
+  if(out == NULL) return 0;
+  int ret_wordcount=0;
+  // va_list p_fmt; 
+  char* s = NULL;
+  int str_len;
+  int d;
+  while(*fmt != '\0') {
+    if(*fmt == '%') {
+      switch (*(++fmt)) {
+        case 'c':
+          d = va_arg(p_fmt, int);
+          memcpy(out, (char*)&d, 1);
+          ++out;
+          ++ret_wordcount;
+          break;
+
+        case 's':
+          s = va_arg(p_fmt, char*);
+          str_len = strlen(s);
+          memcpy(out, s, str_len);
+          out += str_len;
+          ret_wordcount += str_len;
+          break;
+
+        case 'd':
+          d=va_arg(p_fmt, int);
+          str_len = strlen(itos_dec(d, out));
+          out += str_len;
+          ret_wordcount += str_len;
+          break;
+
+        default: break;
+      }
+      fmt++;
+    }
+    else {
+      *out++ = *fmt++;
+      ++ret_wordcount;
+    }
+  }
+  *out = '\0';
+  return ret_wordcount;
+}
+
 // sprintf()  write output  to  the  given  output  stream
 int sprintf(char *out, const char *fmt, ...) {
   if(out == NULL) return 0;
@@ -176,8 +219,66 @@ int snprintf(char *out, size_t n, const char *fmt, ...) {
   return ret_wordcount;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  return 0;
+int vsnprintf(char *out, size_t n, const char *fmt, va_list p_fmt) {
+  if(out == NULL || n-- <=0) return 0;
+  int ret_wordcount=0;
+  char* s = NULL;
+  int str_len;
+  int d;
+  char d_buf[12];
+  while(*fmt != '\0') {
+    if(*fmt == '%') {
+      switch (*(++fmt)) {
+        case 'c':
+          d = va_arg(p_fmt, int);
+          memcpy(out, (char*)&d, 1);
+          ++out;
+          ++ret_wordcount;
+          break;
+
+        case 's':
+          s = va_arg(p_fmt, char*);
+          str_len = strlen(s);
+          if(ret_wordcount + str_len > n) {
+            d = n - ret_wordcount;
+            memcpy(out, s, d);
+            *(out+d+1) = '\0';
+            return n;
+          }
+          memcpy(out, s, str_len);
+          out += str_len;
+          ret_wordcount += str_len;
+          break;
+
+        case 'd':
+          d=va_arg(p_fmt, int);
+          str_len = strlen(itos_dec(d, d_buf));
+          if(ret_wordcount + str_len > n) {
+            d = n - ret_wordcount;
+            memcpy(out, d_buf, d);
+            *(out+d+1) = '\0';
+            return n;
+          }
+          memcpy(out, d_buf, str_len);
+          out += str_len;
+          ret_wordcount += str_len;
+          break;
+
+        default: break;
+      }
+      fmt++;
+    }
+    else {
+      *out++ = *fmt++;
+      ++ret_wordcount;
+    }
+    if(ret_wordcount >= n) {
+      *out = '\0';
+      return ret_wordcount;
+    }
+  }
+  *out = '\0';
+  return ret_wordcount;
 }
 
 #endif
