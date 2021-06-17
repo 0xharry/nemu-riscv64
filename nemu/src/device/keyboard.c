5ee5854f24b84c1bb83e5ad08abdc669c9893335
tracer-ics2020 <tracer@njuics.org>
2021-06-17 20:38:38 +0800

    >  compile ustc    Static hostname: HASEE          Icon name: computer-laptop            Chassis: laptop         Machine ID: 028aba91a4974ce08e9cf5508e313209            Boot ID: 68ed866fc95d401ea00ace905d80a3d7   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  20:38:38 up 1 day,  2:12,  1 user,  load average: 0.88, 0.71, 0.70

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -42,6 +42,7 @@ void send_key(uint8_t scancode, bool is_keydown) {
       keymap[scancode] != _KEY_NONE) {
     uint32_t am_scancode = keymap[scancode] | (is_keydown ? KEYDOWN_MASK : 0);
     key_queue[key_r] = am_scancode;
+    printf("key_queue[key_r(%d)] = am_scancode(%u)\n", key_r, am_scancode);
     key_r = (key_r + 1) % KEY_QUEUE_LEN; // rear入队
     Assert(key_r != key_f, "key queue overflow!");
   }
