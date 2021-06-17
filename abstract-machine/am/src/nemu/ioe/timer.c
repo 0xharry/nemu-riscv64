723abe48f59b1457307bd884a17a2e246751ed72
tracer-ics2020 <tracer@njuics.org>
2021-06-17 14:32:23 +0800

    >  run ustc    Static hostname: HASEE          Icon name: computer-laptop            Chassis: laptop         Machine ID: 028aba91a4974ce08e9cf5508e313209            Boot ID: 68ed866fc95d401ea00ace905d80a3d7   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  14:32:23 up 20:06,  1 user,  load average: 0.29, 0.42, 0.45

--- abstract-machine/am/src/nemu/ioe/timer.c
+++ abstract-machine/am/src/nemu/ioe/timer.c
@@ -6,14 +6,15 @@ void __am_timer_init() {
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uptime->us = 0;
-  // todo
+  // uptime->us = seconds * 1000000 + (useconds + 500);
+  putch((char) uptime->us);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->second = 0;
   rtc->minute = 0;
   rtc->hour   = 0;
-  rtc->day    = 0;
-  rtc->month  = 0;
-  rtc->year   = 1900;
+  rtc->day    = 9;
+  rtc->month  = 8;
+  rtc->year   = 1998;
 }
