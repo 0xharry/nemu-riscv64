02c3ccc664c601b7587ecd2f0c20590e72411bfe
tracer-ics2020 <tracer@njuics.org>
2021-06-17 21:23:25 +0800

    >  run ustc    Static hostname: HASEE          Icon name: computer-laptop            Chassis: laptop         Machine ID: 028aba91a4974ce08e9cf5508e313209            Boot ID: 68ed866fc95d401ea00ace905d80a3d7   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  21:23:25 up 1 day,  2:57,  1 user,  load average: 0.57, 0.47, 0.69

--- abstract-machine/am/src/nemu/ioe/input.c
+++ abstract-machine/am/src/nemu/ioe/input.c
@@ -8,7 +8,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t am_scancode = inl(KBD_ADDR);
   kbd->keydown = (am_scancode & KEYDOWN_MASK ? true : false);
   kbd->keycode = am_scancode & ~KEYDOWN_MASK;
-  if(kbd->keycode != 0) {
-printf("input.c: %s\t(%u)\n", kbd->keydown ? "down" : "up", kbd->keycode);
-  }
+//   if(kbd->keycode != 0) {
+// printf("input.c: %s\t(%u)\n", kbd->keydown ? "down" : "up", kbd->keycode);
+//   }
 }
