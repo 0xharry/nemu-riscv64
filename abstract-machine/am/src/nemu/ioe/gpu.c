140e41bb045ca96ecf41483cd7bc63df4ca91747
tracer-ics2020 <tracer@njuics.org>
2021-06-18 14:56:49 +0800

    >  run ustc    Static hostname: HASEE          Icon name: computer-laptop            Chassis: laptop         Machine ID: 028aba91a4974ce08e9cf5508e313209            Boot ID: 68ed866fc95d401ea00ace905d80a3d7   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-55-generic       Architecture: x86-64  14:56:49 up 1 day, 20:30,  1 user,  load average: 0.83, 0.54, 0.42

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -34,15 +34,15 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 // sync==1时，向屏幕(x,y)坐标处绘制w*h的矩形图像。
 // 图像像素按行优先方式存储在pixels中, 每个像素用32位整数以00RRGGBB的方式描述颜色.
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  uint32_t *pixels = ctl->pixels;
-  int cp_bytes = sizeof(uint32_t) * min(w, W - x); // pixels一行的大小
-  for (int j = 0; j < h && y + j < H; j ++) {      // copy h行
-    for(int bias=0; bias<cp_bytes; ++bias) {
-      outl(FB_ADDR + (y + j) * W + x + bias, pixels[bias]);
-    }
-    pixels += w;
-  }
+  // int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  // uint32_t *pixels = ctl->pixels;
+  // int cp_bytes = sizeof(uint32_t) * min(w, W - x); // pixels一行的大小
+  // for (int j = 0; j < h && y + j < H; j ++) {      // copy h行
+  //   for(int bias=0; bias<cp_bytes; ++bias) {
+  //     outl(FB_ADDR + (y + j) * W + x + bias, pixels[bias]);
+  //   }
+  //   pixels += w;
+  // }
 
   // if (ctl->sync) {
     outl(SYNC_ADDR, 1);
