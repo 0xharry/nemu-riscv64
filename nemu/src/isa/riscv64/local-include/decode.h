6fe6b91a5447303c36aa0f716edea03ffa85de55
tracer-ics2020 <tracer@njuics.org>
2021-07-13 15:41:45 +0800

    >  compile ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: 20c360bec361496897322651f4445b2f   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-59-generic       Architecture: x86-64  15:41:45 up  2:56,  1 user,  load average: 0.47, 0.45, 0.46

--- nemu/src/isa/riscv64/local-include/decode.h
+++ nemu/src/isa/riscv64/local-include/decode.h
@@ -55,11 +55,11 @@ static inline def_DHelper(R) {
 }
 
 static inline def_DHelper(B) {
-  decode_op_r(s, id_src1, s->isa.instr.s.rs1, true);
+  decode_op_r(s, id_src1, s->isa.instr.b.rs1, true);
   sword_t simm = (s->isa.instr.b.simm12  << 12)|
                  (s->isa.instr.b.imm11   << 11)|
                  (s->isa.instr.b.imm10_5 << 5) |
                  (s->isa.instr.b.imm4_1  << 1) ;
   decode_op_i(s, id_src2, simm, true);
-  decode_op_r(s, id_dest, s->isa.instr.s.rs2, true);
+  decode_op_r(s, id_dest, s->isa.instr.b.rs2, true);
 }
