6a10791da8b944a3c986f9c3f6bc8df15da12574
tracer-ics2020 <tracer@njuics.org>
2021-07-14 15:10:12 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: a1910d26d2424732b490eb2e6d7e0dca   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-59-generic       Architecture: x86-64  15:10:12 up 17:30,  1 user,  load average: 0.18, 0.27, 0.32

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -102,10 +102,11 @@ void* memcpy(void* restrict_dst, const void* restrict_src, size_t n) {
   // char* p_out = restrict_dst;
   int i;
   int rem = n%SZ_LONG;
-  for(i=0; i<n; i+=SZ_LONG) {
+  n/=SZ_LONG;
+  for(i=0; i<n; ++i) {
     ((long*)restrict_dst)[i] = ((long*)restrict_src)[i];
   }
-  for(i=rem; i<n; ++i) {
+  for(; i<n+rem; ++i) {
     ((char*)restrict_dst)[i] = ((char*)restrict_src)[i];
   }
   return restrict_dst;
