1e545394611db9adc1156973276286f12adccffb
tracer-ics2020 <tracer@njuics.org>
2021-08-01 20:50:12 +0800

    >  compile ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: dc086b638be1465fb0523edcbed2cd66   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  20:50:12 up 1 min,  1 user,  load average: 2.27, 1.21, 0.47

--- nemu/src/isa/riscv64/intr.c
+++ nemu/src/isa/riscv64/intr.c
@@ -2,15 +2,12 @@
 #include "local-include/rtl.h"
 
 void raise_intr(DecodeExecState *s, word_t NO, vaddr_t epc) {
-  /* TODO: Trigger an interrupt/exception with ``NO''.
-   * That is, use ``NO'' to index the IDT.
-   */
 // 将当前PC值保存到sepc寄存器--pc
+  cpu.csr.sepc = cpu.pc;
 // 在 scause 寄存器中设置异常号--NO
+  cpu.csr.scause = NO;
 // 从 stvec 寄存器中取出异常入口地址
 // 跳转到异常入口地址-- j (stvec)
-  cpu.csr.sepc = cpu.pc;
-  cpu.csr.scause = NO;
   rtl_j(s, cpu.csr.stvec);
 }
 
