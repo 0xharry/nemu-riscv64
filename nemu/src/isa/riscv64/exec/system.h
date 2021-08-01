3b25f4ce18d1b2ef61cdc68a81fec97c5ad8a9f1
tracer-ics2020 <tracer@njuics.org>
2021-08-01 20:03:39 +0800

    >  compile ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: 3e76e48e02b24b34a077b95605bb57dd   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  20:03:39 up  5:43,  1 user,  load average: 0.18, 0.52, 0.65

--- nemu/src/isa/riscv64/exec/system.h
+++ nemu/src/isa/riscv64/exec/system.h
@@ -1,21 +1,37 @@
 // I-TYPE : csr(imm), rs1, rd
 // ddest, dsrc1, id_src2->simm
 // t = CSRs[csr]; CSRs[csr] = t | x[rs1]; x[rd] = t
-#define CASE_CSR_DO(num,str)                    \
+#define CASE_CSR_DO_s(num,str)         \
 case num: *s0 = cpu.csr.str;           \
           cpu.csr.str = *s0|*dsrc1;    \
+          break;
+
+#define CASE_CSR_DO_w(num,str)         \
+case num: *ddest = cpu.csr.str;        \
+          cpu.csr.str = *dsrc1;        \
           break;
 
 static inline def_EHelper(csrrs) {
   switch (id_src2->imm) {
     // CASE_CSR_DO(0x100,sstatus)
-    CASE_CSR_DO(0x105,stvec)
-    CASE_CSR_DO(0x141,sepc)
-    CASE_CSR_DO(0x142,scause)
+    CASE_CSR_DO_s(0x105,stvec)
+    CASE_CSR_DO_s(0x141,sepc)
+    CASE_CSR_DO_s(0x142,scause)
     default: break;
   }
   *ddest = *s0;
-  print_asm_template2(csrrs);
+  print_asm_template3(csrrs);
+}
+
+static inline def_EHelper(csrrw) {
+  switch (id_src2->imm) {
+    // CASE_CSR_DO(0x100,sstatus)
+    CASE_CSR_DO_w(0x105,stvec)
+    CASE_CSR_DO_w(0x141,sepc)
+    CASE_CSR_DO_w(0x142,scause)
+    default: break;
+  }
+  print_asm_template3(csrrs);
 }
 
 // RaiseException(EnvironmentCall)
