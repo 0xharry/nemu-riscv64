47b528c19cfe39baf29ea1e6838a79c955fc1b4f
tracer-ics2020 <tracer@njuics.org>
2021-08-02 21:44:15 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: dc086b638be1465fb0523edcbed2cd66   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  21:44:15 up 1 day, 55 min,  1 user,  load average: 1.86, 0.91, 0.56

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,14 +13,26 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
-static char* header_buffer[sizeof(Elf_Ehdr)];
+static char* elf_buffer[sizeof(Elf_Ehdr)];
+static char* pgm_buffer[sizeof(Elf_Phdr)];
 static Elf_Ehdr* elf_header;
-static size_t elf_offset;
+static Elf_Phdr* pgm_header;
 static uintptr_t loader(PCB *pcb, const char *filename) {
-// read ELF header, start at ${ramdisk_start}
-  elf_offset = ramdisk_read(header_buffer, 0, sizeof(Elf_Ehdr));
-  elf_header = (Elf_Ehdr*)header_buffer;
+// read ELF header, start at ${ramdisk_start} also 0
+  ramdisk_read(elf_buffer, 0, sizeof(Elf_Ehdr));
+  elf_header = (Elf_Ehdr*)elf_buffer;
   assert(*(uint32_t *)elf_header->e_ident == 0x464c457f);
+
+// read program headers, start at elf_header->e_phoff
+  ramdisk_read(pgm_buffer, elf_header->e_phoff, sizeof(Elf_Phdr));
+  pgm_header = (Elf_Phdr*)pgm_buffer;
+  assert(elf_header->e_phentsize == sizeof(Elf_Phdr));
+  assert(elf_header->e_ehsize == sizeof(Elf_Ehdr));
+  // // analyze each program header
+  // for(int i=0; i<elf_header->e_phnum; ++i, pgm_header+=elf_header->e_shentsize){
+    
+  // }
+  
   return elf_header->e_entry;
 }
 
