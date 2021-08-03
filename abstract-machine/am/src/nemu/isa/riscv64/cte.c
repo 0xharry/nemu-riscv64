5644a03abaaef62d97a35e0415d3ed7d2f47bbb9
tracer-ics2020 <tracer@njuics.org>
2021-08-03 21:54:43 +0800

    >  compile ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: dc086b638be1465fb0523edcbed2cd66   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  21:54:43 up 2 days,  1:06,  1 user,  load average: 1.60, 1.04, 1.12

--- abstract-machine/am/src/nemu/isa/riscv64/cte.c
+++ abstract-machine/am/src/nemu/isa/riscv64/cte.c
@@ -10,7 +10,7 @@ Context* __am_irq_handle(Context *c) {
     Event ev = {0};
     switch (c->cause) {
       // case EVENT_NULL: break;
-      // case EVENT_YIELD:   ev.event = EVENT_YIELD;   break;
+      case EVENT_YIELD:   ev.event = EVENT_YIELD;   break;
       // case EVENT_SYSCALL: ev.event = EVENT_SYSCALL; break;
       // case EVENT_PAGEFAULT: break;
       // case EVENT_IRQ_TIMER: break;
@@ -28,14 +28,11 @@ extern void __am_asm_trap(void);
 
 // 进行CTE相关的初始化操作, CTE将会把事件和相关的上下文作为参数,
 // 接受一个来自操作系统的事件处理回调函数的指针
-// (当发生事件时, CTE将会把事件和相关的上下文作为参数, 调用这个回调函数, 交由操作系统进行后续处理.)
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
-  // 设置异常入口地址: 将异常入口地址设置到stvec寄存器
   asm volatile("csrw stvec, %0" : : "r"(__am_asm_trap));
 
   // register event handler 
-  // 注册一个事件处理回调函数, 由Nanos-lite提供
   user_handler = handler;
 
   return true;
