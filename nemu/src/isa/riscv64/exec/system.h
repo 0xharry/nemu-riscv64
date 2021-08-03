6446f44a1b94bc26c3b5b7b8194205c6d02f9f59
tracer-ics2020 <tracer@njuics.org>
2021-08-03 20:18:50 +0800

    >  compile ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: dc086b638be1465fb0523edcbed2cd66   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  20:18:51 up 1 day, 23:30,  1 user,  load average: 1.68, 1.38, 1.09

--- nemu/src/isa/riscv64/exec/system.h
+++ nemu/src/isa/riscv64/exec/system.h
@@ -43,6 +43,6 @@ static inline def_EHelper(ecall) {
 // ExceptionReturn(Supervisor)
 static inline def_EHelper(sret) {
   Log("sret to %p", (void*)cpu.csr.sepc);
-  cpu.pc = cpu.csr.sepc;
+  rtl_j(s, cpu.csr.sepc);
   print_asm_template1(sret);
 }
