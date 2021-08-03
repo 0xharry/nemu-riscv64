b4146886ade6df8f98855cef18b01a01feb6bbaa
tracer-ics2020 <tracer@njuics.org>
2021-08-03 10:13:06 +0800

    >  compile ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: dc086b638be1465fb0523edcbed2cd66   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  10:13:06 up 1 day, 13:24,  1 user,  load average: 1.20, 0.90, 0.80

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,6 +1,6 @@
 #include <proc.h>
 #include <elf.h>
-
+// #include "/home/harry/ics2020/nemu/include/memory/paddr.h"
 #ifdef __LP64__
 # define Elf_Ehdr Elf64_Ehdr
 # define Elf_Phdr Elf64_Phdr
@@ -17,6 +17,7 @@ static char* elf_buffer[sizeof(Elf_Ehdr)];
 static char* pgm_buffer[sizeof(Elf_Phdr)];
 static Elf_Ehdr* elf_header;
 static Elf_Phdr* pgm_header;
+// static const int zero_buf;
 static uintptr_t loader(PCB *pcb, const char *filename) {
 // read ELF header, start at ${ramdisk_start} also 0
   ramdisk_read(elf_buffer, 0, sizeof(Elf_Ehdr));
@@ -26,13 +27,20 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 // read program headers, start at elf_header->e_phoff
   ramdisk_read(pgm_buffer, elf_header->e_phoff, sizeof(Elf_Phdr));
   pgm_header = (Elf_Phdr*)pgm_buffer;
-  assert(elf_header->e_phentsize == sizeof(Elf_Phdr));
-  assert(elf_header->e_ehsize == sizeof(Elf_Ehdr));
-  // // analyze each program header
-  // for(int i=0; i<elf_header->e_phnum; ++i, pgm_header+=elf_header->e_shentsize){
-    
-  // }
-  Log("yes, indeed");
+
+  // analyze each program header
+  for(int i=0; i<elf_header->e_phnum; ++i, pgm_header+=sizeof(Elf_Phdr)){
+    // .bss set to zero
+    assert((unsigned long)pgm_header->p_offset==(unsigned long)pgm_header);
+    Log("yes indeed");
+    // ramdisk_write((const void*)&zero_buf, pgm_header->p_offset,
+    //                 pgm_header->p_memsz - pgm_header->p_filesz);
+    // // write into memery
+    // for(int j=0; j<pgm_header->p_memsz; j+=pgm_header->p_align)
+    //   paddr_write(pgm_header->p_vaddr+j,
+    //               ramdisk_read(pgm_buffer,pgm_header+j,pgm_header->p_align),
+    //               pgm_header->p_align);
+  }
   return elf_header->e_entry;
 }
 
