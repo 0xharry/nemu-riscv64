f24f0a44a130a641c5bdd9236c9464e946525761
tracer-ics2020 <tracer@njuics.org>
2021-08-04 17:03:46 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: dc086b638be1465fb0523edcbed2cd66   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  17:03:46 up 2 days, 20:15,  1 user,  load average: 0.56, 0.61, 0.66

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,17 +9,17 @@ void do_syscall(Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-
-  for(int i=0; i<31; ++i) {
-    printf("x%d : %p\n", i+1, (void*)c->gpr[i]);
-  }
+  
+  // for(int i=0; i<31; ++i) {
+  //   printf("x%d : %p\n", i+1, (void*)c->gpr[i]);
+  // }
 
   switch (a[0]) {
-    case SYS_exit :  call_exit(c, a[0], a[1], a[2]);  break;
-    case SYS_yield:  call_yield(c, a[0], a[1], a[2]); break;
+    case SYS_exit :  call_(exit);  break;
+    case SYS_yield:  call_(yield); break;
     // case SYS_open :
     // case SYS_read :
-    case SYS_write:  call_write(c, a[0], a[1], a[2]); break;
+    case SYS_write:  call_(write); break;
     case SYS_close:
     case SYS_lseek:
     case SYS_brk  :
