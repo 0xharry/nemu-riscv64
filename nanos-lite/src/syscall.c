5c49cb06ae94685ac141cc70d5a921a0c05dae05
tracer-ics2020 <tracer@njuics.org>
2021-08-04 13:57:25 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: dc086b638be1465fb0523edcbed2cd66   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  13:57:26 up 2 days, 17:09,  1 user,  load average: 0.27, 0.54, 0.63

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,14 @@
 #include <common.h>
 #include "syscall.h"
+#include "/home/harry/ics2020/navy-apps/libs/libos/src/syscall.h"
+#include "/home/harry/ics2020/abstract-machine/am/include/am.h"
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
+    case SYS_yield: yield();
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
