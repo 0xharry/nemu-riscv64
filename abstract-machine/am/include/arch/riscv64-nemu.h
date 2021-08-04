8781a2d32c1bd05525a55286fea2f613a80e4d30
tracer-ics2020 <tracer@njuics.org>
2021-08-04 14:08:07 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: dc086b638be1465fb0523edcbed2cd66   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  14:08:07 up 2 days, 17:19,  1 user,  load average: 1.18, 0.85, 0.72

--- abstract-machine/am/include/arch/riscv64-nemu.h
+++ abstract-machine/am/include/arch/riscv64-nemu.h
@@ -6,7 +6,7 @@ struct Context {
   uintptr_t gpr[31], cause, status, epc;
 };
 
-#define GPR1 gpr[17] // a7
+#define GPR1 gpr[16] // a7
 #define GPR2 gpr[0]
 #define GPR3 gpr[0]
 #define GPR4 gpr[0]
