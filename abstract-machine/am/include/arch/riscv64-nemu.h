fbef40a8bb747481706eec59e30001a5b48e2453
tracer-ics2020 <tracer@njuics.org>
2021-08-04 14:12:39 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: dc086b638be1465fb0523edcbed2cd66   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  14:12:40 up 2 days, 17:24,  1 user,  load average: 1.02, 1.06, 0.85

--- abstract-machine/am/include/arch/riscv64-nemu.h
+++ abstract-machine/am/include/arch/riscv64-nemu.h
@@ -6,10 +6,10 @@ struct Context {
   uintptr_t gpr[31], cause, status, epc;
 };
 
-#define GPR1 gpr[16] // a7
-#define GPR2 gpr[0]
-#define GPR3 gpr[0]
-#define GPR4 gpr[0]
-#define GPRx gpr[0]
+#define GPR1 gpr[16]  // a7
+#define GPR2 gpr[9]   // a0
+#define GPR3 gpr[10]  // a1
+#define GPR4 gpr[11]  // a2
+#define GPRx gpr[9]   // a0
 
 #endif
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,9 +6,9 @@
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  for(int i=0; i<30; ++i) {
-    printf("%d : %p\n", i, (void*)c->gpr[i]);
-  }
+  // for(int i=0; i<30; ++i) {
+  //   printf("%d : %p\n", i, (void*)c->gpr[i]);
+  // }
 
   switch (a[0]) {
     case SYS_yield: yield(); break;
