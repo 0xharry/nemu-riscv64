cf2a3c76b93aa6e0a806c02a2eb40d9e7259d5cc
tracer-ics2020 <tracer@njuics.org>
2021-08-05 13:30:56 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: a1cb1627104c4190a7a8ea0e67c2800f   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  13:30:56 up  4:18,  1 user,  load average: 0.43, 0.57, 0.77

--- abstract-machine/am/src/nemu/isa/riscv64/cte.c
+++ abstract-machine/am/src/nemu/isa/riscv64/cte.c
@@ -1,3 +1,4 @@
+// CTE(Context Extension) - 上下文扩展, 为程序提供上下文管理的能力
 #include <am.h>
 #include <nemu.h>
 #include <klib.h>
@@ -5,27 +6,32 @@
 static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
-
   if (user_handler) {
+    c->epc += 4;
     Event ev = {0};
+    // if(c->gpr[16]<=19 && c->gpr[16]>=0) ev.event = EVENT_SYSCALL;
+    // else if(c->gpr[16]==-1)             ev.event = EVENT_YIELD;
+    // else                                ev.event = EVENT_ERROR;
     switch (c->cause) {
-      case 9 : ev.event = ((c->GPR1==1)?EVENT_YIELD:EVENT_SYSCALL); c->epc+=4; break; 
-      default: ev.event = EVENT_ERROR; break;
+      case  9: ev.event = EVENT_SYSCALL; break; // syscall number $a7, arguments $a0~5
+      default: ev.event = EVENT_ERROR;   break;
     }
-
     c = user_handler(ev, c);
     assert(c != NULL);
   }
-  
+
   return c;
 }
 
 extern void __am_asm_trap(void);
 
+// 进行CTE相关的初始化操作, CTE将会把事件和相关的上下文作为参数,
+// 接受一个来自操作系统的事件处理回调函数的指针
 bool cte_init(Context*(*handler)(Event, Context*)) {
   // initialize exception entry
   asm volatile("csrw stvec, %0" : : "r"(__am_asm_trap));
-  // register event handler
+
+  // register event handler 
   user_handler = handler;
 
   return true;
@@ -36,7 +42,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
 }
 
 void yield() {
-  asm volatile("li a7, -1; ecall");
+  asm volatile("li a7, 1; ecall");
 }
 
 bool ienabled() {
@@ -45,4 +51,3 @@ bool ienabled() {
 
 void iset(bool enable) {
 }
-
