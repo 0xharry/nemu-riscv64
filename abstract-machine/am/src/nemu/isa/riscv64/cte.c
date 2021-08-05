18e1cf9341320325638104b344d31d0a475d5066
tracer-ics2020 <tracer@njuics.org>
2021-08-05 13:31:43 +0800

    >  compile ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: a1cb1627104c4190a7a8ea0e67c2800f   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  13:31:43 up  4:19,  1 user,  load average: 0.26, 0.50, 0.74

--- abstract-machine/am/src/nemu/isa/riscv64/cte.c
+++ abstract-machine/am/src/nemu/isa/riscv64/cte.c
@@ -7,7 +7,7 @@ static Context* (*user_handler)(Event, Context*) = NULL;
 
 Context* __am_irq_handle(Context *c) {
   if (user_handler) {
-    c->epc += 4;
+    // c->epc += 4;
     Event ev = {0};
     // if(c->gpr[16]<=19 && c->gpr[16]>=0) ev.event = EVENT_SYSCALL;
     // else if(c->gpr[16]==-1)             ev.event = EVENT_YIELD;
