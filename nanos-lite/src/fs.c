53ea965f08c2d9302fd06e03fd4ca703ec74b74b
tracer-ics2020 <tracer@njuics.org>
2021-08-07 11:02:29 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: 41deeaca570644adba14dc144d89e224   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  11:02:29 up 1 day, 17:42,  1 user,  load average: 1.23, 0.65, 0.49

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,24 +1,25 @@
 #include <fs.h>
+#include "/home/harry/ics2020/abstract-machine/am/include/am.h"
+#define putnstr(buf, n) \
+    for(size_t i=0; i<n; ++i) putch(*(char*)(buf+i))
+
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+extern size_t get_ramdisk_size();
 
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
-extern size_t serial_write(const void *buf, size_t offset, size_t len);
-extern size_t events_read(void *buf, size_t offset, size_t len);
-extern size_t dispinfo_read(void *buf, size_t offset, size_t len);
-
-extern size_t fb_write(const void *buf, size_t offset, size_t len); 
-extern size_t dispinfo_write(const void *buf,size_t offset, size_t len) ;
 typedef struct {
-  char *name;    //文件名
-  size_t size;  //文件大小
-  size_t disk_offset; //文件在ramdisk中的偏移
-  size_t open_offset;  //新增！相对于单个文件的偏移
+  char *name;           // fixed in nanos's initialization
+  size_t size;          // fixed in nanos's initialization
+  size_t disk_offset;   // fixed in nanos's initialization
   ReadFn read;
-  WriteFn write;  
+  WriteFn write;
+  size_t file_offset;   // offset for each program file, start at 0;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_DEV, FD_DISP, FD_FB};
+enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -32,146 +33,73 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin",      0, 0, 0, invalid_read,  invalid_write},
-  [FD_STDOUT] = {"stdout",     0, 0, 0, invalid_read,  serial_write},
-  [FD_STDERR] = {"stderr",     0, 0, 0, invalid_read,  serial_write},
-  [FD_DEV]    = {"/dev/events",0, 0, 0, invalid_read,   invalid_write},
-  [FD_DISP]   = {"/proc/dispinfo", 0, 0, 0, invalid_read, invalid_write},
-  [FD_FB]     = {"/dev/fb",    0, 0, 0, invalid_read,  invalid_write},
+  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
 };
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
-  int w = io_read(AM_GPU_CONFIG).width;
-  int h = io_read(AM_GPU_CONFIG).height;
-  file_table[FD_FB].size = w*h*4;
 }
 
-int    fs_open(const char *pathname, int flags, int mode);
-size_t fs_read(int fd, void *buf, size_t len);
-size_t fs_write(int fd, const void *buf, size_t len);
-size_t fs_lseek(int fd, size_t offset, int whence);
-int    fs_close(int fd);
+#define total_file_num sizeof(file_table)/sizeof(Finfo)
+size_t fs_lseek(int fd, size_t offset, int whence) {
+  switch (whence){
+  case SEEK_SET:
+    assert(file_table[fd].size <= offset);
+    file_table[fd].file_offset = offset;
+    return offset;
 
-extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
-extern size_t ramdisk_write(const void *buf, size_t offset, size_t len) ;
-char buf[4096];
+  case SEEK_CUR:
+    file_table[fd].file_offset += offset;
+    assert(file_table[fd].file_offset <= file_table[fd].size);
+    return file_table[fd].file_offset;
 
+  case SEEK_END: // static file-system, do not support SEEK_END
+  default: assert(0);
+  }
+}
 
-
-//打开文件并返回对应的文件描述符
 int fs_open(const char *pathname, int flags, int mode) {
-  //如何找到pathname? 在file_table中找
-  //忽略权限位 flag
-  //使用ramdisk_read 进行真正的读取
-  //偏移量不要越界
-  
-  // Finfo * ff;
-  
-  //printf("get in fs_open\n");
-  bool find = false;
-  int sum = sizeof(file_table)/sizeof(Finfo);
-  //printf("sum is %d\n",sum);
-  int i,ret = 0;
-  for(i=0;i<sum;i++) {
-    //printf("第%d个 : %s\n",i,file_table[i].name);
-    if(strcmp(pathname,file_table[i].name)==0) {
-      file_table[i].open_offset = 0;
-      if(i>=6){
-        file_table[i].read  = ramdisk_read;
-        file_table[i].write = ramdisk_write;
+  for(int fd=0; fd<total_file_num; ++fd)
+    if(strcmp(pathname, file_table[fd].name)==0) {
+      file_table[fd].file_offset = 0;
+      if(fd > 2) {
+        file_table[fd].read  = ramdisk_read;
+        file_table[fd].write = ramdisk_write;
       }
-      find = true;
-      ret = i;
-      break;
+printf("[fs_open]\n file %d: \"%s\"\n size: %p\n", fd, pathname, file_table[fd].size);
+      return fd;
     }
-  }  
-  assert(find==true);
-  return ret;
+
+  assert(0);
 }
 
-//从文件描述符中进行读取
-//偏移量不能越界
 size_t fs_read(int fd, void *buf, size_t len) {
-  //printf("get in fs_read\n");
-  Finfo * ff = &file_table[fd];
-  //printf("ff-addr : %p, buf-addr: %p, len-addr: %p\n",ff,buf,&len);
-  assert(ff);
+  if(fd<=2) return 0; // ignore stdin, stdout and stderr
 
-  // 读指针越界 || 读指针地址+读取长度 越界
-  //printf("ff->open_offset : %d,  ff->size : %d\n",ff->open_offset,ff->size);
-  assert( ff->open_offset <= ff->size );
+printf("[fs_read] fd %d\n from (%u + offset %d)\n size=%p\n",\
+        fd, file_table[fd].disk_offset, file_table[fd].file_offset, len);
 
-  // if(ff->open_offset + len > ff->size) {
-  //   len = ff->size - ff->open_offset;
-  //   assert( len <= ff->size );
-  // }
-  /* read `len' bytes starting from `offset' of ramdisk into `buf' */
-  //printf("disk_offset is 0x%x\n",ff->disk_offset);
-  size_t count = ff->read(buf,ff->disk_offset + ff->open_offset,len);
-  fs_lseek(fd,count,SEEK_CUR); //注意更新!
-  return count;
+  file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].file_offset, len);
+  return fs_lseek(fd, len, SEEK_CUR);
 }
 
+size_t fs_write(int fd, const void *buf, size_t len) {
+  if(fd==1 || fd==2) {
+    putnstr(buf, len);
+    return len;
+  }
+  if(fd==0) return 0;
 
-// size_t fs_read_all(int fd,void *buf) {
-//   return fs_read(fd, buf, file_table[fd].size);
-// }
+printf("[fs_write] fd %d\n from (%u + offset %d)\n size=%p\n",\
+        fd, file_table[fd].disk_offset, file_table[fd].file_offset, len);
+
+  file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].file_offset, len);
+  return fs_lseek(fd, len, SEEK_CUR);
+}
 
 int fs_close(int fd) {
-  file_table[fd].open_offset = 0;
   return 0;
-}
-
-
-// buf -> fd
-size_t fs_write(int fd, const void *buf, size_t len) {
-  
-  Finfo * ff = &file_table[fd];
-  assert(ff);
-  // printf("ff-addr : %p, buf-addr: %p\n",ff,buf);
-  // 写指针越界 || 读写针地址+写长度 越界
-  // printf("fd is %d\n",fd);
-  // printf("ff->open_offset : %d,  ff->size : %d\n",ff->open_offset,ff->size);
-
-  // if(fd==1 || fd==2) {
-  //   int i;
-  //   for(i=0;i<len;i++) {
-  //     putch(*(char *)(buf+i));
-  //   }
-  //   return len;
-  // }
-  // else {
-    //assert( ff->open_offset <= ff->size );
-
-  // if(ff->open_offset + len > ff->size) {
-  //   len = ff->size - ff->open_offset;
-  //   assert( ff->open_offset <= ff->size );
-  // }
-    /* write `len' bytes starting from `buf' into the `offset' of ramdisk */
-    size_t count = ff->write(buf,ff->disk_offset + ff->open_offset,len);
-
-  // int i;
-  // for(i=0;i<count;i++) {
-  //   putch(*(char *)(buf+i));
-  // }
-    fs_lseek(fd,count,SEEK_CUR);
-    return count;
-  // }
-}
-
-//根据whence修改offset
-//为每一个已经打开的文件引入偏移量属性open_offset, 
-//来记录目前文件操作的位置. 每次对文件读写了多少个字节, 偏移量就前进多少.
-size_t fs_lseek(int fd, size_t offset, int whence) {
-  Finfo * ff = &file_table[fd];
-
-  switch(whence) {
-    case SEEK_SET: ff->open_offset = offset;break;
-    case SEEK_CUR: ff->open_offset +=offset;break;
-    case SEEK_END: ff->open_offset = ff->size + offset;break;
-    default: assert(0);
-  }
-  return ff->open_offset;
 }
