0939561da76af846f2f01a0cde0ea47b4bdea127
tracer-ics2020 <tracer@njuics.org>
2021-08-08 15:28:37 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: c54e31a9f0364eb8973e442868c42073   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  15:28:38 up  6:26,  1 user,  load average: 2.98, 1.80, 1.12

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,7 @@
 #include <common.h>
 #include "syscall.h"
 #include "/home/harry/ics2020/abstract-machine/am/include/am.h"
+#include <sys/time.h>
 #define _a7  c->GPR1
 #define _a0  c->GPR2
 #define _a1  c->GPR3
@@ -12,6 +13,14 @@ extern size_t fs_write(int fd, const void *buf, size_t len);
 extern size_t fs_lseek(int fd, size_t offset, int whence);
 // extern int fs_close(int fd);
 
+int tm_get(struct timeval *tv, struct timezone *tz) {
+  static uint64_t us;
+  ioe_read(AM_TIMER_UPTIME, &us);
+  tv->tv_sec  = us / (uint64_t)10e6;
+  tv->tv_usec = us % (uint64_t)10e6;
+  return 0;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = _a7;
@@ -20,16 +29,16 @@ void do_syscall(Context *c) {
   a[3] = _a2;
 
   switch (a[0]) {
-    case SYS_exit :  halt(_a0);                              break;
-    case SYS_yield:  yield();                                break;
-    case SYS_open :  _ret = fs_open ((char*)_a0, _a1, _a2);  break;
-    case SYS_read :  _ret = fs_read (_a0, (char*)_a1, _a2);  break;
-    case SYS_write:  _ret = fs_write(_a0, (char*)_a1, _a2);  break;
-    case SYS_close:  _ret = 0;                               break;
-    case SYS_lseek:  _ret = fs_lseek(_a0, _a1, _a2);         break;
-    case SYS_brk  :  _ret = 0;                               break;
-    // case SYS_execve:
-    // case SYS_gettimeofday:
+    case SYS_exit :  halt(_a0);                                break;
+    case SYS_yield:  yield();                                  break;
+    case SYS_open :  _ret = fs_open ((char*)_a0, _a1, _a2);    break;
+    case SYS_read :  _ret = fs_read (_a0, (char*)_a1, _a2);    break;
+    case SYS_write:  _ret = fs_write(_a0, (char*)_a1, _a2);    break;
+    case SYS_close:  _ret = 0;                                 break;
+    case SYS_lseek:  _ret = fs_lseek(_a0, _a1, _a2);           break;
+    case SYS_brk  :  _ret = 0;                                 break;
+    case SYS_gettimeofday: 
+                     _ret = tm_get((struct timeval*)_a0,NULL); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
