90a194f29440b8f6c5da57911c633b4828a8f1d7
tracer-ics2020 <tracer@njuics.org>
2021-08-08 18:03:41 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: c54e31a9f0364eb8973e442868c42073   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  18:03:41 up  9:01,  1 user,  load average: 1.13, 1.04, 0.94

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -9,14 +9,21 @@ static int fbdev = -1;
 static int screen_w = 0, screen_h = 0;
 
 uint32_t NDL_GetTicks() {
-  return 0;
+  struct timeval tv;
+  gettimeofday(&tv,NULL);
+  //转换成微秒
+  return tv.tv_sec*1000000 + tv.tv_usec;
 }
 
 int NDL_PollEvent(char *buf, int len) {
-  return 0;
+  int fd = open("/dev/events",0,0);
+  return read(fd,buf,len);
 }
 
+// 打开一张(*w) X (*h)的画布
+// 如果*w和*h均为0, 则将系统全屏幕作为画布, 并将*w和*h分别设为系统屏幕的大小
 void NDL_OpenCanvas(int *w, int *h) {
+  printf("get in opencanvas\n");
   if (getenv("NWM_APP")) {
     int fbctl = 4;
     fbdev = 5;
@@ -33,10 +40,34 @@ void NDL_OpenCanvas(int *w, int *h) {
       if (strcmp(buf, "mmap ok") == 0) break;
     }
     close(fbctl);
+  }else {
+    // 如果*w和*h均为0, 则将系统全屏幕作为画布, 并将*w和*h分别设为系统屏幕的大小
+    if (*w == 0 && *h == 0) {
+      *w = 400; *h = 300;
+    }
+    //解析出屏幕大小
+    screen_w = *w;
+    screen_h = *h;
+    // printf("width : %d, height : %d\n",screen_w,screen_h);
+    char buf[64];
+    int len = sprintf(buf, "WIDTH: %d\nHEIGHT: %d\n", screen_w, screen_h);
+    write(4, buf, 64);
   }
 }
 
+// 向画布`(x, y)`坐标处绘制`w*h`的矩形图像, 并将该绘制区域同步到屏幕上
+// 图像像素按行优先方式存储在`pixels`中, 每个像素用32位整数以`00RRGGBB`的方式描述颜色
 void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+  printf("get in drawrect\n");
+  int fd = open("/dev/fb",0,0);
+  printf("width : %d, height : %d\n",screen_w,screen_h);
+  w = (x+w>screen_w)?screen_w-x:w;
+  h = (y+h>screen_h)?screen_h-y:h;
+  int i;
+  for(i=0;i<h;i++) {
+    lseek(fd, ((y+i)*screen_w+x)*4,0);
+    write(fd, pixels + i * w, w * 4);
+  }
 }
 
 void NDL_OpenAudio(int freq, int channels, int samples) {
