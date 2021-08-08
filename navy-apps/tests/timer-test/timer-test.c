5f4a50cd0faea9384553d0a912bd917a99632226
tracer-ics2020 <tracer@njuics.org>
2021-08-08 17:01:22 +0800

    >  compile ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: c54e31a9f0364eb8973e442868c42073   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  17:01:22 up  7:59,  1 user,  load average: 0.83, 0.67, 0.63

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -12,12 +12,13 @@ extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 int main() {
   struct timeval t;
   _syscall_(SYS_gettimeofday, 0, &t, 0);
-  int half_sec_glass=(t.tv_usec / 500000 < 1);
-  while(1) {
-    _syscall_(SYS_gettimeofday, 0, &t, 0);
-    half_sec_glass ^= (t.tv_usec / 500000 < 1);
-    while(half_sec_glass) {
-      printf("一句话\n");
+  double sec = t.tv_sec;
+  int half_sec_glass=((t.tv_usec / 500000) == 1);
+  while(!_syscall_(SYS_gettimeofday, 0, &t, 0)) {
+    half_sec_glass ^= ((t.tv_usec / 500000) == 1);
+    if((long)sec <= t.tv_sec && half_sec_glass) {
+      printf("一句话, sec=%d, half_sec_glass=%d\n", (long)sec, t.tv_usec);
+      sec += 0.5;
     }
   }
   return 0;
