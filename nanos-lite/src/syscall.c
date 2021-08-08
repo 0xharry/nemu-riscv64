6ca10e19aa25f411f8039637d25260b475afa886
tracer-ics2020 <tracer@njuics.org>
2021-08-08 09:41:00 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: c54e31a9f0364eb8973e442868c42073   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  09:41:00 up 39 min,  1 user,  load average: 0.36, 0.43, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,8 +12,8 @@ extern size_t fs_read(int fd, void *buf, size_t len);
 extern size_t fs_write(int fd, const void *buf, size_t len);
 extern size_t fs_lseek(int fd, size_t offset, int whence);
 extern int    fs_close(int fd);
-size_t _write(int fd, void *buf, size_t count);
-int sys_gettimeofday(struct timeval *tv, struct timezone *tz);
+// size_t _write(int fd, void *buf, size_t count);
+// int sys_gettimeofday(struct timeval *tv, struct timezone *tz);
 
 
 void do_syscall(Context *c) {
@@ -31,24 +31,24 @@ void do_syscall(Context *c) {
     case SYS_close: c->GPRx = fs_close(a[1]);                  break;   
     case SYS_lseek: c->GPRx = fs_lseek(a[1],a[2],a[3]);        break;
     case SYS_brk  : c->GPRx = 0;                               break;
-    case SYS_gettimeofday : c->GPRx = sys_gettimeofday((struct timeval *)a[1],(struct timezone *)a[2]);break;
+    // case SYS_gettimeofday : c->GPRx = sys_gettimeofday((struct timeval *)a[1],(struct timezone *)a[2]);break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
 
 
 
-size_t _write(int fd, void *buf, size_t count){
-  //size_t ret = ramdisk_write(buf,0,count);
-  size_t ret = count;
-  if(fd == 1 || fd == 2) {
-    int i;
+// size_t _write(int fd, void *buf, size_t count){
+//   //size_t ret = ramdisk_write(buf,0,count);
+//   size_t ret = count;
+//   if(fd == 1 || fd == 2) {
+//     int i;
 
-    for(i=0;i<count;i++) {
-      putch(*(char *)(buf+i));
-    }
-    return ret;
-  }
-  return -1;
-}
+//     for(i=0;i<count;i++) {
+//       putch(*(char *)(buf+i));
+//     }
+//     return ret;
+//   }
+//   return -1;
+// }
 
