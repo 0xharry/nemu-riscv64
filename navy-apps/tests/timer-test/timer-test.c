3032b279b47b6a9cc6385ad3c38f257bdfec2b5f
tracer-ics2020 <tracer@njuics.org>
2021-08-08 18:01:29 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: c54e31a9f0364eb8973e442868c42073   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  18:01:29 up  8:59,  1 user,  load average: 1.34, 1.10, 0.95

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -10,26 +10,26 @@
 extern int _syscall_(int, uintptr_t, uintptr_t, uintptr_t);
 
 int main() {
-  // struct timeval t;
-  // _syscall_(SYS_gettimeofday, 0, &t, 0);
-  // int sec = t.tv_sec;
-  // int half_sec_glass=((t.tv_usec / 500000) == 1);
-  // while(!_syscall_(SYS_gettimeofday, 0, &t, 0)) {
-  //   half_sec_glass ^= ((t.tv_usec / 500000) == 1);
-  //   if((long)sec <= t.tv_sec) {
-  //     printf("一句话, sec=%d, half_sec_glass=%d\n", sec, t.tv_usec);
-  //     sec++;
-  //   }
-  // }
   struct timeval t;
-  int sec = 1;
-  _syscall_(SYS_gettimeofday, &t, 0, 0);
-  int base = t.tv_sec;
-  while (1) {
-    _syscall_(SYS_gettimeofday, &t, 0, 0);
-    while(t.tv_sec-base < sec);
-    printf("一句话\n");
-    sec++;
+  _syscall_(SYS_gettimeofday, 0, &t, 0);
+  int sec = t.tv_sec;
+  int half_sec_glass=((t.tv_usec / 500000) == 1);
+  while(!_syscall_(SYS_gettimeofday, 0, &t, 0)) {
+    half_sec_glass ^= ((t.tv_usec / 500000) == 1);
+    if((long)sec <= t.tv_sec) {
+      printf("一句话, sec=%d, half_sec_glass=%d\n", sec, t.tv_usec);
+      sec++;
+    }
   }
+  // struct timeval t;
+  // int sec = 1;
+  // _syscall_(SYS_gettimeofday, &t, 0, 0);
+  // int base = t.tv_sec;
+  // while (1) {
+  //   _syscall_(SYS_gettimeofday, &t, 0, 0);
+  //   while(t.tv_sec-base < sec);
+  //   printf("一句话\n");
+  //   sec++;
+  // }
   return 0;
 }
