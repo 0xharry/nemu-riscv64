b60dc10ceae997ec0c6d56fc9dc473cfc51dcd06
tracer-ics2020 <tracer@njuics.org>
2021-08-08 18:15:21 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: c54e31a9f0364eb8973e442868c42073   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  18:15:21 up  9:13,  1 user,  load average: 1.46, 1.06, 0.98

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -8,10 +8,7 @@ int main() {
   int half_sec_glass=((t.tv_usec / 500000) == 1);
   while(!gettimeofday(&t,NULL)) {
     half_sec_glass ^= ((t.tv_usec / 500000) == 1);
-    if((long)sec <= t.tv_sec) {
-      printf("一句话 ");
-      sec++;
-    }
+    printf("一句话 ");
   }
   return 0;
 }
