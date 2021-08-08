927604fd5b41f2df4b0152c719e8903d9f1acc1f
tracer-ics2020 <tracer@njuics.org>
2021-08-08 10:47:30 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: c54e31a9f0364eb8973e442868c42073   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  10:47:30 up  1:45,  1 user,  load average: 1.63, 1.46, 1.06

---
+++ nanos-lite/src_old/device.c
@@ -0,0 +1,43 @@
+#include <common.h>
+
+#if defined(MULTIPROGRAM) && !defined(TIME_SHARING)
+# define MULTIPROGRAM_YIELD() yield()
+#else
+# define MULTIPROGRAM_YIELD()
+#endif
+
+#define NAME(key) \
+  [AM_KEY_##key] = #key,
+
+static const char *keyname[256] __attribute__((used)) = {
+  [AM_KEY_NONE] = "NONE",
+  AM_KEYS(NAME)
+};
+
+#define putnstr(buf, n) \
+    for(size_t i=0; i<n; ++i) putch(*(const char*)(buf+i))
+size_t serial_write(const void *buf, size_t offset, size_t len) {
+  putnstr("[stdout/err] len:", 18);
+  putch('0'+len/10);
+  putch('0'+len%10);
+  putch('\t');
+  putnstr(buf, len);
+  return len;
+}
+
+size_t events_read(void *buf, size_t offset, size_t len) {
+  return 0;
+}
+
+size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+  return 0;
+}
+
+size_t fb_write(const void *buf, size_t offset, size_t len) {
+  return 0;
+}
+
+void init_device() {
+  Log("Initializing devices...");
+  ioe_init();
+}
--- nanos-lite/src_old/fs.c
+++ nanos-lite/src_old/fs.c
@@ -1,12 +1,10 @@
 #include <fs.h>
 #include "/home/harry/ics2020/abstract-machine/am/include/am.h"
-#define putnstr(buf, n) \
-    for(size_t i=0; i<n; ++i) putch(*(const char*)(buf+i))
 
 extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();
-
+extern size_t serial_write(const void *buf, size_t offset, size_t len);
 typedef size_t (*ReadFn) (void *buf, size_t offset, size_t len);
 typedef size_t (*WriteFn) (const void *buf, size_t offset, size_t len);
 
@@ -34,8 +32,8 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
@@ -47,13 +45,13 @@ void init_fs() {
 size_t fs_lseek(int fd, size_t offset, int whence) {
   switch (whence){
   case SEEK_SET:
-    assert(file_table[fd].size >= offset);
+    // assert(file_table[fd].size >= offset);
     file_table[fd].file_offset = offset;
     break;
 
   case SEEK_CUR:
     file_table[fd].file_offset += offset;
-    assert(file_table[fd].file_offset <= file_table[fd].size);
+    // assert(file_table[fd].file_offset <= file_table[fd].size);
     break;
 
   case SEEK_END:
@@ -92,21 +90,10 @@ size_t fs_read(int fd, void *buf, size_t len) {
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
-  if(fd==1 || fd==2) {
-    // putnstr("[stdout/err] len:", 18);
-    // putch('0'+len/10);
-    // putch('0'+len%10);
-    // putch('\t');
-    putnstr(buf, len);
-    return len;
-  }
-  if(fd==0) return 0;
-
   file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].file_offset, len);
   return fs_lseek(fd, len, SEEK_CUR);
 }
 
 int fs_close(int fd) {
-  file_table[fd].file_offset = 0;
   return 0;
 }
