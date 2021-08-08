aecbe63144653a63511775be1c39655d40e8da17
tracer-ics2020 <tracer@njuics.org>
2021-08-08 18:11:40 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: c54e31a9f0364eb8973e442868c42073   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  18:11:40 up  9:09,  1 user,  load average: 0.71, 0.83, 0.91

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -2,17 +2,16 @@
 #include <sys/time.h>
 
 int main() {
-    struct timeval time;
-    int sec = 1;
-    int j = 0;
-    while (1) {
-        j++;
-        if (j == 100000) {
-            _syscall_(19, &time, 0, 0);
-            int pre_usec = time.tv_usec;
-            printf("sec : %ld, usec : %ld\n",time.tv_sec, time.tv_usec);
-            j = 0;
-        }
+  struct timeval t;
+  gettimeofday(&t,NULL);
+  int sec = t.tv_sec;
+  int half_sec_glass=((t.tv_usec / 500000) == 1);
+  while(!gettimeofday(&t,NULL)) {
+    half_sec_glass ^= ((t.tv_usec / 500000) == 1);
+    if((long)sec <= t.tv_sec) {
+      printf("一句话, sec=%d, half_sec_glass=%d\n", sec, t.tv_usec);
+      sec++;
     }
-    return 0;
+  }
+  return 0;
 }
