92c0edb9b3cd6eb95a790a77b1e33dd0b2783e8a
tracer-ics2020 <tracer@njuics.org>
2021-08-08 18:56:05 +0800

    >  run ustc    Static hostname: hasee          Icon name: computer-laptop            Chassis: laptop         Machine ID: 916ce0add599458ea2beadd4def37578            Boot ID: c54e31a9f0364eb8973e442868c42073   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.11.0-25-generic       Architecture: x86-64  18:56:05 up  9:54,  1 user,  load average: 1.47, 1.05, 0.76

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -3,26 +3,12 @@
 #include <NDL.h>
 
 int main() {
-//   NDL_Init(0);
-//   unsigned ms=NDL_GetTicks();
-//   while(1) {
-//       while(NDL_GetTicks() < ms);
-//       ms+=10;
-//       printf("一句话");
-//   }
-//   return 0;
-    struct timeval time;
-    int halfsec = 1;
-    int j = 0;
-    while(1) {
-        j++;
-        if(j==100000) {
-            //因为不精确，所以不能整除来求
-            int cur = NDL_GetTicks()/500000;
-            if(cur >= halfsec) {
-                printf("0.5s pass\n");
-                halfsec++;
-            }
-        }
-    }
+  NDL_Init(0);
+  unsigned ms=NDL_GetTicks();
+  while(1) {
+      while(NDL_GetTicks() < ms);
+      ms+=1000;
+      printf("一句话\n");
+  }
+  return 0;
 }
